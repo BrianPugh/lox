e8e299da9f3b41dfeac1b0f3485bd52c0b1c1e40
Brian Pugh <bnp117@gmail.com>
2019-05-23 04:14:47 -0700

    OneWriterManyReader documentation

--- lox/lock/OneWriterManyReader.py
+++ lox/lock/OneWriterManyReader.py
@@ -1,5 +1,6 @@
 """@package OneWriterManyReader
 """
+
 import threading
 from contextlib import contextmanager
 from lox.lock.LightSwitch import LightSwitch
@@ -7,24 +8,37 @@ from lox.lock.LightSwitch import LightSwitch
 __all__ = ["OneWriterManyReader",]
 
 class OneWriterManyReader:
-    """
-    Lock for a One-writer-Many-reader scenario.
+    """Lock for a One-writer-Many-reader scenario.
 
-    Starvation policy: Once the writer attempts to acquire the lock, all further
-    reads will block until the writer acquires and subsequently releaases the lock.
+    Unlimited numbers of reader can obtain the lock, but as soon as a writer
+    attempts to acquire the lock, all reads are blocked until the current readers
+    are finished, the writer acquires the lock, and finally releases it.
+
+    Attributes
+    ----------
+    read_counter : int
+        Number of readers that have acquired the lock.
+
+    Methods
+    -------
+    acquire(rw_flag:str)
+        Acquire the lock as a "reader" or a "writer"
+
+    release(rw_flag:str)
+        Release the lock as a "reader" or a "writer"
+
     """
     def __init__(self):
         self._no_writers    = threading.Lock()
         self._no_readers    = threading.Lock()
-        self._read_counter  = LightSwitch(self._no_writers)
+        self.read_counter  = LightSwitch(self._no_writers)
         self._write_counter = LightSwitch(self._no_readers)
         self._readers_queue = threading.Lock()
 
     @contextmanager
     def __call__(self, rw_flag:str, timeout=-1):
-        """
-        Only call as part of a "with" statement.
-        """
+        """ Used in contextmanager to specify acquire/release type """
+
         self.acquire(rw_flag, timeout=timeout)
         try:
             yield self
@@ -32,9 +46,13 @@ class OneWriterManyReader:
             self.release(rw_flag)
 
     def __len__(self):
+        """Get the read_counter value"""
+
         return len(self.read_counter)
 
     def _check_rw_flag(self, rw_flag):
+        """Checks if passed in flag is a valid value"""
+
         rw_flag = rw_flag.lower()
         if rw_flag == 'r':
             pass
@@ -45,16 +63,26 @@ class OneWriterManyReader:
         return rw_flag
 
     def acquire(self, rw_flag:str, timeout=-1):
-        """ Acquire the lock either as a reader or a writer.
-        @param rw_flag Either 'r' or 'w' for read/write acquire
-        @param timeout Time in seconds before timeout occurs for acquiring lock
-        @return True if lock was acquired; False otherwise
+        """ Acquire the lock as a "reader" or a "writer".
+
+        Parameters
+        ----------
+        rw_flag : str
+            Either 'r' for 'read' or 'w' for 'write' acquire.
+
+        timeout : float
+            TODO: Time in seconds before timeout occurs for acquiring lock.
+
+        Returns
+        -------
+            True if lock was acquired, False otherwise
         """
+
         rw_flag = self._check_rw_flag(rw_flag)
         if rw_flag == 'r':
             with self._readers_queue:
                 with self._no_readers:
-                    self._read_counter.acquire()
+                    self.read_counter.acquire()
         elif rw_flag == 'w':
             self._write_counter.acquire()
             self._no_writers.acquire()
@@ -62,11 +90,16 @@ class OneWriterManyReader:
 
     def release(self, rw_flag:str):
         """Release acquired lock
-        @param rw_flag Either 'r' or 'w' for read/write acquire
+
+        Parameters
+        ----------
+        rw_flag : str
+            Either 'r' for 'read' or 'w' for 'write' acquire.
         """
+
         rw_flag = self._check_rw_flag(rw_flag)
         if rw_flag == 'r':
-            self._read_counter.release()
+            self.read_counter.release()
         elif rw_flag == 'w':
             self._no_writers.release()
             self._write_counter.release()
