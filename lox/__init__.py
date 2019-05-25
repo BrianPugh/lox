144c79c1bc3d7e0436a3cbdfdcb89d2b60c03eb5
Brian Pugh <bnp117@gmail.com>
2019-05-25 13:54:04 -0700

    Bump version: 0.2.0 → 0.2.1

--- lox/__init__.py
+++ lox/__init__.py
@@ -6,7 +6,7 @@ Easy multithreading for every project.
 
 __author__ = """Brian Pugh"""
 __email__ = 'bnp117@gmail.com'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 from lox.lock import *
 from lox.worker import *
