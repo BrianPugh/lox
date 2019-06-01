b7d3cb3d814688045bdb65e957c446f3e6ba7230
Brian Pugh <bnp117@gmail.com>
2019-06-01 15:55:54 -0700

    Bump version: 0.2.1 → 0.3.0

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/BrianPugh/lox',
-    version='0.2.1',
+    version='0.3.0',
     zip_safe=False,
 )
