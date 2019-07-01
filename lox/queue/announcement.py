b7ede7f79690d8d86a707b79b0ef5de1f637ff1c
Brian Pugh <bnp117@gmail.com>
2019-06-30 17:43:31 -0700

    Added backlog to Announcement

--- lox/queue/announcement.py
+++ lox/queue/announcement.py
@@ -6,11 +6,19 @@
 
 import queue
 from collections import deque
+import threading
 
-__all__ = ["Announcement",]
+__all__ = ["Announcement", "SubscribeFinalizedError", ]
+
+class SubscribeFinalizedError(Exception):
+    """ Raised when an Announcement.subscribe has been called,
+    but the Announcement has already been finalized.
+    """
+
+    pass
 
 class Announcement:
-    """ Put to many queues.
+    """ Put to many queues with backlog support.
 
     Example:
 
@@ -37,20 +45,67 @@ class Announcement:
         >>> bar.scatter()
         >>> bar_res = bar.gather()
 
+    The backlog allows future (or potentially race-condition) subscribers to 
+    get content put'd before they subscribed.
+
+    Attributes
+    ----------
+    backlog : deque
+        Backlog of queued data. None if not used.
     """
 
-    def __init__(self, maxsize=0):
-        """
+    @property
+    def final(self,):
+        return self._final
+
+    @final.setter
+    def final(self, val : bool):
+        with self.lock:
+            self._final = val
+
+    @property
+    def backlog(self,):
+        return self._backlog
+
+    @backlog.setter
+    def backlog(self, val):
+        self._backlog = val
+
+    def __init__(self, maxsize=0, backlog=None):
+        """ Create an Announcement object.
 
         Parameters
         ----------
         maxsize : int
-            Created queue's maximum size. Defaults to 0 (no size limit)
+            Created queue's default maximum size. Defaults to 0 (no size limit)
 
+        backlog : int
+            Create a log of previously put data. New subscribers get the entire 
+            backlog pushed onto their queue. Memory usage potentially unbounded.
+
+            A value <=0 specifies unbounded backlog size. A value >0 specifies 
+            a circular buffer.
+
+            Defaults to None (backlog not used).
+
+            Note: If used, it is recommended to use the ``finalize()`` method so
+            that the backlog can be free'd when no longer necessary.
         """
-
+        # General Attributes #
         self.subscribers = deque()
         self.maxsize = maxsize
+        self.lock = threading.RLock()
+
+        # Backlog objects #
+        self.backlog_use = False if backlog is None else True
+        self.final = False
+        if self.backlog_use:
+            if backlog <= 0:
+                backlog = None
+            self.backlog = deque(maxlen=backlog)
+        else:
+            self.backlog = None
+
 
     def __len__(self,):
         """ Get the number of subscribers.
@@ -71,11 +126,13 @@ class Announcement:
         data : object
             Data to put on the queue
         """
+        with self.lock:
+            for q in self.subscribers:
+                q.put(data, *args, **kwargs)
+            if self.backlog_use and not self.final:
+                self.backlog.append(data)
 
-        for q in self.subscribers:
-            q.put(data, *args, **kwargs)
-
-    def subscribe(self, q=None, maxsize=None):
+    def subscribe(self, q=None, maxsize=None, *args, **kwargs):
         """ Subscribe to announcements
 
         Parameters
@@ -94,11 +151,19 @@ class Announcement:
             Queue-like object.
         """
 
-        if q is None:
-            if maxsize is None:
-                maxsize = self.maxsize
-            q = queue.Queue( maxsize=maxsize )
-        self.subscribers.append(q)
+        with self.lock:
+            if self.final:
+                raise SubscribeFinalizedError
+
+            if q is None:
+                # Create Queue
+                if maxsize is None:
+                    maxsize = self.maxsize
+                q = queue.Queue( maxsize=maxsize )
+            self.subscribers.append(q)
+            if self.backlog_use:
+                for x in self.backlog:
+                    q.put(x, *args, **kwargs)
         return q
 
     def unsubscribe(self, q):
@@ -115,4 +180,17 @@ class Announcement:
             ``q`` was not a subscriber.
         """
 
-        self.subscribers.remove(q)
+        with self.lock:
+            self.subscribers.remove(q)
+
+    def finalize(self,):
+        """ Do not allow any more subscribers.
+
+        Only used for memory efficiency if backlog is used.
+        """
+
+        self.final = True
+        if not self.backlog_use:
+            return
+        self.backlog = None
+
