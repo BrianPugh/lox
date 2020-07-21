68f975df882e31f9166e92be109fa77369495a34
Brian Pugh <bnp117@gmail.com>
2020-07-20 21:12:43 -0700

    remove dead code

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,10 +1,5 @@
 import lox
-from lox.helper import term_colors, cdf, cdfi
-from time import sleep, time
-import sys
-import gc
-
-SLEEP_TIME = 0.01
+from lox.helper import term_colors
 
 
 def test_ctx(capsys):
@@ -26,31 +21,3 @@ def test_ctx(capsys):
             print("test header")
 
 
-class Class1:
-
-    def __init__(self, z):
-        self.z = z
-
-    @lox.thread(2)
-    def test_method1(self, x, y):
-        sleep(SLEEP_TIME)
-        return x * y + self.z
-
-    @lox.thread
-    def test_method2(self, x, y):
-        sleep(SLEEP_TIME)
-        return x * y - self.z
-
-
-def test_MethodDecoratorAdaptor_gc():
-    len_cdfi = len(cdfi)
-
-    for i in range(100):
-        obj = Class1(i)
-        obj.test_method1.scatter(i, i+1)
-        obj.test_method1.gather()
-        del obj
-    gc.collect()
-
-    # cdfi should be as it previously was, otherwise gc isn't working
-    assert(len_cdfi == len(cdfi))
