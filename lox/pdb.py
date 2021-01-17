6bc5e1efdbeca35c6c80ba62518108959f07eb14
Brian Pugh <bnp117@gmail.com>
2021-01-17 12:16:54 -0800

    more careful on acquiring/releasing mutex

--- lox/pdb.py
+++ lox/pdb.py
@@ -12,15 +12,34 @@ except ImportError:
     import pdb
 
 _pdb_lock = threading.Lock()
+_in_session = False
 
 class _Pdb(pdb.Pdb):
-    def preloop(self):
-        _pdb_lock.acquire()
-        return super().preloop()
+    def __init__(self, completekey='tab', stdin=None, stdout=None, skip=None,
+                     nosigint=False, readrc=True):
+        super().__init__(completekey=completekey, stdin=stdin, stdout=stdout, skip=skip,
+                     nosigint=nosigint, readrc=readrc)
+        self.prompt = '(LoxPdb) '
 
-    def postloop(self):
+    def set_continue(self):
+        global _in_session
+        _in_session = False
         _pdb_lock.release()
-        return super().postloop()
+        super().set_continue()
+
+    def set_quit(self):
+        global _in_session
+        _in_session = False
+        _pdb_lock.release()
+        super().set_quit()
+
+    def set_trace(self, frame=None):
+        global _in_session
+        if not _in_session:
+            _pdb_lock.acquire()
+        _in_session = True
+        res = super().set_trace(frame)
+        return res
 
 def set_trace(*, header=None):
     debugger = _Pdb()
--- tests/test_pdb.py
+++ tests/test_pdb.py
@@ -21,9 +21,45 @@ def test_pdb_set_trace_manual():
     """ No mocking for manual testing to confirm our mocks are appropriate """
     lox.set_trace()
 
-@pytest.mark.skip(reason="Not Implemented")
-def test_pdb_set_trace_multi(mock_pdb_lock, mock_cmd_continue):
-    pass
+def test_pdb_set_trace_multi_1(mock_pdb_lock, mock_cmd_continue):
+    lox.set_trace()
+    mock_pdb_lock.acquire.assert_called_once()
+    mock_pdb_lock.release.assert_called_once()
+    lox.set_trace()
+    assert mock_pdb_lock.acquire.call_count == 2
+    assert mock_pdb_lock.release.call_count == 2
+
+def test_pdb_set_trace_multi_2(mocker, mock_pdb_lock):
+    mock_cmd = mocker.patch('cmd.input', side_effect=('n', 'c', 'c', 'c', 'c', 'c'))
+    lox.set_trace()
+    a_noop_statement_that_pdb_will_stop_on_unlike_pass = None  # Will pause here
+    a_noop_statement_that_pdb_will_stop_on_unlike_pass = None  # 'n' brings us here, the 'c' will cause us to release the mutex
+    lox.set_trace()                                            # mutex reacquired
+    a_noop_statement_that_pdb_will_stop_on_unlike_pass = None  # will pause here, the 'c' will cause us to release the mutex
+    a_noop_statement_that_pdb_will_stop_on_unlike_pass = None
+    assert mock_pdb_lock.acquire.call_count == 2
+    assert mock_pdb_lock.release.call_count == 2
+
+def test_pdb_set_trace_multi_3(mocker, mock_pdb_lock):
+    mock_cmd = mocker.patch('cmd.input', side_effect=('n', 'n', 'c', 'c', 'c', 'c', 'c'))
+    lox.set_trace()
+    a_noop_statement_that_pdb_will_stop_on_unlike_pass = None
+    lox.set_trace()                                           # should not be reacquired since we're already debugging
+    a_noop_statement_that_pdb_will_stop_on_unlike_pass = None
+    a_noop_statement_that_pdb_will_stop_on_unlike_pass = None
+    assert mock_pdb_lock.acquire.call_count == 1
+    assert mock_pdb_lock.release.call_count == 1
+
+def test_pdb_set_trace_multi_4(mocker, mock_pdb_lock):
+    mock_cmd = mocker.patch('cmd.input', side_effect=('n', 'n', 'c', 'c', 'c', 'c', 'c'))
+    lox.set_trace()  # obtain
+    lox.set_trace()  # not obtained since we're in session
+    lox.set_trace()  # not obtained since we're in session
+    lox.set_trace()  # Released and reobtained because of 'c'
+    lox.set_trace()  # Released and reobtained because of 'c'
+    assert mock_pdb_lock.acquire.call_count == 3
+    assert mock_pdb_lock.release.call_count == 3
+
 
 @pytest.mark.skip(reason="Not Implemented")
 def test_pdb_set_trace_multithread_basic(mock_pdb_lock, mock_cmd_continue):
