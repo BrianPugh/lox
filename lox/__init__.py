0e8f4221e11d8e943f25bbd76a8486cae3fa4b26
Brian Pugh <bnp117@gmail.com>
2023-01-27 18:12:37 -0800

    modernize project to https://github.com/BrianPugh/python-template

--- lox/__init__.py
+++ lox/__init__.py
@@ -6,7 +6,8 @@ Easy multithreading for every project.
 
 __author__ = """Brian Pugh"""
 __email__ = "bnp117@gmail.com"
-__version__ = "0.11.0"
+# Don't manually change, let poetry-dynamic-versioning-plugin handle it.
+__version__ = "0.0.0"
 
 import lox.worker
 from lox.worker.process import process
