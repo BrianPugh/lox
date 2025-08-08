8262dcd1e8d67dc94d68f00c3638ba06415ad223
Brian Pugh <bnp117@gmail.com>
2025-08-08 16:11:54 -0400

    Fix docs errors.

--- docs/conf.py
+++ docs/conf.py
@@ -121,7 +121,6 @@ html_theme = "sphinx_rtd_theme"
 html_theme_options = {
     "canonical_url": "",
     "logo_only": True,
-    "display_version": False,
     "prev_next_buttons_location": "bottom",
     "style_external_links": False,
     "style_nav_header_background": "white",
