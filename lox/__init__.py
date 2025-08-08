40e21430aa4cdb2c18f133d376c8e2e74046512c
Brian Pugh <bnp117@gmail.com>
2025-08-08 15:50:35 -0400

    Bump version: 0.13.0 → 1.0.0

--- lox/__init__.py
+++ lox/__init__.py
@@ -6,7 +6,7 @@ Easy multithreading for every project.
 
 __author__ = """Brian Pugh"""
 __email__ = "bnp117@gmail.com"
-__version__ = "0.13.0"
+__version__ = "1.0.0"
 
 __all__ = [
     "Announcement",
